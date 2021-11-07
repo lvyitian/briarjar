0819ad3817a0405035448c4d60eccc72803ed359
alex <3100-user@users.noreply.code.briarproject.org>
2021-11-08 00:24:13 +0100

    Update the very basic Dagger example (to demonstrate how it works).

--- src/main/java/org/briarjar/briarjar/experimental/DependsComponent.java
+++
@@ -1,8 +0,0 @@
-package org.briarjar.briarjar.experimental;
-
-import dagger.Component;
-
-@Component
-public interface DependsComponent {
-    Depends getDepends();
-}
---
+++ src/main/java/org/briarjar/briarjar/experimental/daggerExamples/firstSimpleExample/Main.java
@@ -0,0 +1,21 @@
+package org.briarjar.briarjar.experimental.daggerExamples.firstSimpleExample;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// This object is desired
+		HasDependencies hasDependencies;
+
+		// This component offers it
+		ExampleOfDependenciesComponent exampleOfDependenciesComponent;
+
+
+		// Creation of component (create() used for this simple example)
+		exampleOfDependenciesComponent = DaggerExampleOfDependenciesComponent.create();
+
+		hasDependencies = exampleOfDependenciesComponent.getHasDependencies();
+		hasDependencies.method();
+
+	}
+}
